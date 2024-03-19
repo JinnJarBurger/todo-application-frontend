486c67aeedfe416fd68d61410c53fe54c23e7636
Adnan Hossain <adnanhossain274@gmail.com>
2024-03-19 22:31:14 +0600

    Added Major Components && Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {LoginComponent} from "./login/login.component";
+import {FooterComponent} from "./footer/footer.component";
+import {MenuComponent} from "./menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent, LoginComponent],
+  imports: [RouterOutlet, WelcomeComponent, LoginComponent, FooterComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,12 +1,32 @@
 import {Component} from '@angular/core';
+import {DatePipe, NgForOf, UpperCasePipe} from "@angular/common";
+
+export class Todo {
+
+  constructor(public id: number,
+              public description: string,
+              public done: boolean,
+              public targetDate: Date) {
+  }
+}
 
 @Component({
   selector: 'app-list-todos',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    DatePipe,
+    UpperCasePipe
+  ],
   templateUrl: './list-todos.component.html',
   styleUrl: './list-todos.component.css'
 })
 export class ListTodosComponent {
 
+  todos = [
+    new Todo(1, 'not empty 1', false, new Date()),
+    new Todo(2, 'not empty 2', false, new Date()),
+    new Todo(3, 'not empty 3', false, new Date()),
+    new Todo(4, 'not empty 4', false, new Date())
+  ]
 }
